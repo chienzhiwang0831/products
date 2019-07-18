82499130a8405cb947f678e15fa8c43623ad7986
unknown <csfn05@csf.org.tw>
2019-07-18 17:54:21 +0800

    continue

--- products.py
+++ products.py
@@ -1,4 +1,12 @@
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
 while True:
 	name = input('商品: ')
 	if name == 'q':
