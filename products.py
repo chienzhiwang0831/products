3166bb080e43f66ed239323d3588d8d9bf0fa0fb
unknown <csfn05@csf.org.tw>
2019-07-30 11:38:42 +0800

    check file exist

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
+import os #operating system
+# 讀取檔案
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):# 檢查是否有這檔案
+	print('準備開啟檔案')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')
 
+# 讓使用者輸入
 while True:
 	name = input('商品: ')
 	if name == 'q':
@@ -15,10 +22,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 印出購買記錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
